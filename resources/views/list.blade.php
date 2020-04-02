8e0fb77e891122d83121761fb4a10d7c3ce10009
triest <you@example.com>
2020-04-02 22:59:32 +0300

    Fix style

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -4,7 +4,13 @@
 @section('content')
     <h3>Bid List</h3>
     <b><a href="{{ route('form') }}">Create Bid</a></b>
+    @guest
+        <p>
+            Login for view Bids
+        </p>
+    @else
         <div id="app">
             <listcomponent></listcomponent>
         </div>
+    @endguest
 @endsection
